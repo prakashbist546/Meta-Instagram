c4a33294575e8b531105acde41660d6111003e48
Prakash Bist <prakash.bist@smartsolar.com.np>
2023-07-11 10:14:38 +0900

    Initial Commit

---
+++ Meta-Insta/Meta_InstaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Meta_InstaApp.swift
+//  Meta-Insta
+//
+//  Created by Prakash Bist on 2023/07/11.
+//
+
+import SwiftUI
+
+@main
+struct Meta_InstaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
